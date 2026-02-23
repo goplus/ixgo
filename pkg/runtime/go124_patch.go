10d17fd1da0b98e6aa3a501c86806607bfcaaea7
visualfc <visualfc@gmail.com>
2026-02-23 11:05:18 +0800

    pkg: impl runtime.AddCleanup

--- pkg/runtime/go124_patch.go
+++ pkg/runtime/go124_patch.go
@@ -1,18 +1,24 @@
-//go:build go1.24 && !linknamefix
-// +build go1.24,!linknamefix
+//go:build go1.24
+// +build go1.24
 
 package runtime
 
 import (
 	_ "embed"
-	_ "unsafe"
+	"runtime"
+	"unsafe"
 
 	"github.com/goplus/ixgo"
 )
 
-//go:embed _patch/mcleanup_empty.go
+//go:embed _patch/mcleanup.go
 var patch_data []byte
 
 func init() {
+	ixgo.RegisterExternal("runtime.addCleanup", addCleanup)
 	ixgo.RegisterPatch("runtime", patch_data)
+}
+
+func addCleanup(p unsafe.Pointer, fn func()) runtime.Cleanup {
+	return runtime.AddCleanup[uintptr, int]((*uintptr)(p), func(int) { fn() }, 0)
 }
