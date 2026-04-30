0b706caef20ee5e9d7b2200e4faa61e1715dfa2d
visualfc <visualfc@gmail.com>
2026-04-30 15:21:47 +0800

    load/mod: func fix

--- load/mod/mod.go
+++ load/mod/mod.go
@@ -1,7 +1,6 @@
 package mod
 
 import (
-	"errors"
 	"fmt"
 
 	"golang.org/x/mod/modfile"
@@ -9,16 +8,17 @@ import (
 
 // ParseModFile parse go.mod
 func ParseModFile(file string, data []byte) (*modfile.File, error) {
-	fix := func(path, vers string) (resolved string, err error) {
-		// do nothing
-		return vers, nil
-	}
 	f, err := modfile.Parse(file, data, fix)
 	if err != nil {
-		return nil, fmt.Errorf("parse go.mod %w", err)
+		return nil, fmt.Errorf("failed to parse modfile: %w", err)
 	}
 	if f.Module == nil {
-		return nil, errors.New("no module declaration in go.mod")
+		return nil, fmt.Errorf("no module declaration in %s", file)
 	}
 	return f, nil
+}
+
+func fix(path, vers string) (resolved string, err error) {
+	// do nothing
+	return vers, nil
 }
