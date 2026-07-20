38ff81f1e212ac709d2d39e49d00b17aea805b57
visualfc <visualfc@gmail.com>
2026-07-20 09:40:38 +0800

    llgo: impl reflect.Value.Pointer for FuncForPC

--- runtime_func.go
+++ runtime_func.go
@@ -4,9 +4,38 @@
 package ixgo
 
 import (
+	"path/filepath"
+	"reflect"
 	"runtime"
 	"unsafe"
+
+	"github.com/visualfc/funcval"
 )
+
+func init() {
+	if funcval.IsSupport {
+		RegisterExternal("(reflect.Value).Pointer", func(fr *frame, v reflect.Value) uintptr {
+			if v.Kind() == reflect.Func {
+				if fv, n := funcval.Get(v.Interface()); n == 1 {
+					if c := (*makeFuncVal)(unsafe.Pointer(fv)); c.interp == fr.interp {
+						return uintptr(c.pfn.base)
+					}
+				}
+			}
+			return v.Pointer()
+		})
+		RegisterExternal("(reflect.Value).UnsafePointer", func(fr *frame, v reflect.Value) unsafe.Pointer {
+			if v.Kind() == reflect.Func {
+				if fv, n := funcval.Get(v.Interface()); n == 1 {
+					if c := (*makeFuncVal)(unsafe.Pointer(fv)); c.interp == fr.interp {
+						return unsafe.Pointer(uintptr(c.pfn.base))
+					}
+				}
+			}
+			return v.UnsafePointer()
+		})
+	}
+}
 
 //go:linkname runtimePanic runtime.gopanic
 func runtimePanic(e interface{})
@@ -25,4 +54,25 @@ func inlineFunc(entry uintptr) *funcinl {
 
 func isInlineFunc(f *runtime.Func) bool {
 	return (*funcinl)(unsafe.Pointer(f)).ones == ^uint32(0)
+}
+
+func runtimeFuncFileLine(fr *frame, f *runtime.Func, pc uintptr) (file string, line int) {
+	entry := f.Entry()
+	if isInlineFunc(f) && pc > entry {
+		interp := fr.interp
+		if pfn := findFuncByEntry(interp, int(entry)); pfn != nil {
+			// pc-1 : fn.instr.pos
+			pos := pfn.PosForPC(int(pc - entry - 1))
+			if !pos.IsValid() {
+				return "?", 0
+			}
+			fpos := interp.ctx.FileSet.Position(pos)
+			if fpos.Filename == "" {
+				return "??", fpos.Line
+			}
+			file, line = filepath.ToSlash(fpos.Filename), fpos.Line
+			return
+		}
+	}
+	return f.FileLine(pc)
 }
--- runtime_func_llgo.go
+++ runtime_func_llgo.go
@@ -4,9 +4,52 @@
 package ixgo
 
 import (
+	"go/token"
+	"path/filepath"
+	"reflect"
 	"runtime"
-	_ "unsafe"
+	"unsafe"
 )
+
+func getMakeFuncClosure(v reflect.Value) *makeFuncClosure {
+	pv := (*reflectValue)(unsafe.Pointer(&v))
+	fn := (*llgoClosure)(pv.ptr)
+	return (*makeFuncClosure)(fn.env)
+}
+
+type llgoClosure struct {
+	fn  unsafe.Pointer
+	env unsafe.Pointer
+}
+
+type makeFuncClosure struct {
+	fn  unsafe.Pointer
+	env *struct {
+		interp **Interp
+		pfn    **function
+		typ    *reflect.Type
+		env    *[]interface{}
+	}
+}
+
+func init() {
+	RegisterExternal("(reflect.Value).Pointer", func(fr *frame, v reflect.Value) uintptr {
+		if v.Kind() == reflect.Func {
+			if c := getMakeFuncClosure(v); c != nil && c.env != nil && *c.env.interp == fr.interp {
+				return uintptr((*c.env.pfn).base)
+			}
+		}
+		return v.Pointer()
+	})
+	RegisterExternal("(reflect.Value).UnsafePointer", func(fr *frame, v reflect.Value) unsafe.Pointer {
+		if v.Kind() == reflect.Func {
+			if c := getMakeFuncClosure(v); c != nil && c.env != nil && *c.env.interp == fr.interp {
+				return unsafe.Pointer(uintptr((*c.env.pfn).base))
+			}
+		}
+		return v.UnsafePointer()
+	})
+}
 
 type funcinl struct {
 	entry uintptr
@@ -27,3 +70,29 @@ func isInlineFunc(f *runtime.Func) bool {
 
 //go:linkname runtimePanic github.com/goplus/llgo/runtime/internal/runtime.Panic
 func runtimePanic(e interface{})
+
+func runtimeFuncFileLine(fr *frame, f *runtime.Func, pc uintptr) (file string, line int) {
+	entry := f.Entry()
+	if isInlineFunc(f) && pc >= entry {
+		interp := fr.interp
+		if pfn := findFuncByEntry(interp, int(entry)); pfn != nil {
+			var pos token.Pos
+			if pc == entry {
+				pos = pfn.Fn.Pos()
+			} else {
+				// pc-1 : fn.instr.pos
+				pos = pfn.PosForPC(int(pc - entry - 1))
+				if !pos.IsValid() {
+					return "?", 0
+				}
+			}
+			fpos := interp.ctx.FileSet.Position(pos)
+			if fpos.Filename == "" {
+				return "??", fpos.Line
+			}
+			file, line = filepath.ToSlash(fpos.Filename), fpos.Line
+			return
+		}
+	}
+	return f.FileLine(pc)
+}
