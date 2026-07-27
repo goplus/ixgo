3b7beac3345b88c1882e8107913e3f126746459c
Rick Guo <hello@liuxi.ng>
2026-07-27 15:17:08 +0800

    transform/sccp: remove redundant tests

--- transform/sccp/testdata/sccp/defer-print/in.go
+++
@@ -1,8 +0,0 @@
-package test
-
-func target() int {
-	defer println(42, true, false, true, 1.5, "world", (chan int)(nil), []int(nil), (map[string]int)(nil), (func())(nil), byte(255))
-	defer println(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-	defer print("printing: ")
-	return 0
-}
